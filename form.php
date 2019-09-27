1a02019d1a6e4f77a5b27e400057ffc4e23f7c90
Tony11818 <40446163+Tony11818@users.noreply.github.com>
2019-09-26 19:36:14 -0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,17 @@
+<h2>Give Me Your Info!!!</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  Age: <input type="number" name="age" value="<?php echo $age;?>">
+  <span class="error">* <?php echo $ageErr;?></span>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
